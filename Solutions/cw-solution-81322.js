99dd46d3a14219dd14a3477e400589c01e72d524
robinetta <alxr19@gmail.com>
2022-08-13 14:06:11 -0400

    Daily Code Wars Solution 8-13-22 8kyu

---
+++ Solutions/cw-solution-81322.js
@@ -0,0 +1,25 @@
+// we need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// // SOLUTION
+
+function numberToString(num) {
+    let thisIsAStringNow = num.toString()
+      return thisIsAStringNow 
+    }
+
+// TEST
+
+// const assert = require('chai').assert;
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(numberToString(67), '67');
+//   });
+// });
