d8493dd7db305f47d33455525f1f46458f2ee186
robinetta <alxr19@gmail.com>
2022-08-14 14:10:09 -0400

    Daily Code Wars Solution 8-14-22 8kyu

---
+++ Solutions/cw-solution-81422.js
@@ -0,0 +1,16 @@
+// Write a function which converts the input string to uppercase.
+
+// // SOLUTION
+
+function makeUpperCase(str) {
+    let newStr = str.toUpperCase()
+    return newStr
+  }
+
+// TEST
+
+// describe("Basic Tests", function(){
+//     it("should pass the basic tests", function(){
+//       Test.assertEquals(makeUpperCase("hello"), "HELLO");
+//     });
+//   });
