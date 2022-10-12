b20e4419b489e9bc5ff2332b0831db610df45673
robinetta <alxr19@gmail.com>
2022-10-12 07:58:30 -0400

    codewars solution 10-12-22 8kyu

---
+++ Solutions/cw-solution-101222.js
@@ -0,0 +1,17 @@
+// Bin to Decimal
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+// SOLUTION
+
+function binToDec(bin){
+    return parseInt(bin,2)
+  }
+
+// TEST
+
+// describe("Example Tests", () => {
+//     [ ["1",1], ["0",0], ["1001001", 73] ].forEach( ([inp, exp]) => Test.assertEquals( binToDec(inp), exp ) )
+//   });
+  
