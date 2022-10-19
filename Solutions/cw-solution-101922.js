18048c02f71897849a5eb7897373aad70c68563f
robinetta <alxr19@gmail.com>
2022-10-19 06:39:26 -0400

    daily codewars solution 10-19-22 7kyu

---
+++ Solutions/cw-solution-101922.js
@@ -0,0 +1,32 @@
+// Testing 1-2-3
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+// // // SOLUTION
+
+var number=function(array){
+  return array.map(e,i) => `${i + 1}: ${e}`
+  }
+
+  
+// // // TEST
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.deepEqual(number([]), [], 'Empty array should return empty array');
+//     assert.deepEqual(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"], 'Return the correct line numbers');                
+//   });
+// });
