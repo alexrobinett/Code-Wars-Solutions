8ecc1c2e281084b17c4d046b134e97aeff4a0294
robinetta <alxr19@gmail.com>
2022-11-09 22:05:15 -0500

    daily codewars solutin 11-9-22 8kyu

---
+++ Solutions/cw-solution-11922.js
@@ -0,0 +1,33 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+// SOLUTION
+
+function abbrevName(name){
+    let arr = name.toUpperCase().split(" ")
+    return `${arr[0].charAt(0)}.${arr[1].charAt(0)}`
+  }
+  
+
+// TEST
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+//     assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+//     assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+//     assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+//     assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+//   });
+// });
